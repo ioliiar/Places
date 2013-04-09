2c20c11f6fca46cf48fbb150bac71b2fc2d8639c
serdizzyk <serdizzyk@gmail.com>
2013-04-09 03:53:05 -0700

    Added Place classes

---
+++ Place/PLPlace.m
@@ -0,0 +1,13 @@
+//
+//  PLPlace.m
+//  Place
+//
+//  Created by Serhii on 4/9/13.
+//  Copyright (c) 2013 Iurii Oliiar. All rights reserved.
+//
+
+#import "PLPlace.h"
+
+@implementation PLPlace
+
+@end
---
+++ Place/PLPlaceManager.h
@@ -0,0 +1,13 @@
+//
+//  PLPlaceManager.h
+//  Place
+//
+//  Created by Serhii on 4/9/13.
+//  Copyright (c) 2013 Iurii Oliiar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PLPlaceManager : NSObject
+
+@end
